51ccd126e0cb6b0823b22f8386e1f9c037775657
Benjamin-Endale <onyxbngame@gmail.com>
2025-03-05 20:44:09 +0300

    Printing Object

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const car = {
   brand: "Ford",
   model: "Fiesta",
   color: "blue",
+  Engine: {
+    type: "V8",
+    horsePower: "500",
+  },
 };
 
-console.log(car["brand"]);
+console.log(car.Engine["type"]);
