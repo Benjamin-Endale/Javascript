3753ab1fbf06ef95e15a65237c975d2af7d28e5a
Benjamin-Endale <onyxbngame@gmail.com>
2025-03-18 00:32:20 +0300

    little Fix

--- 2nd_project/Modal.js
+++ 2nd_project/Modal.js
@@ -1,8 +1,13 @@
 const findBtn = document.querySelector(".modal_container");
 const missingBox = document.querySelector(".missing_Container");
 const closeBtn = document.querySelector(".xBtn");
+const anotherBtn = document.querySelector(".FindBtn");
 
 findBtn.addEventListener("click", function () {
+  missingBox.style.display = "flex";
+});
+
+anotherBtn.addEventListener("click", function () {
   missingBox.style.display = "flex";
 });
 
