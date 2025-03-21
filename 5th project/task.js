01b479aece54513ca66e7459b631ede644c0be83
Benjamin-Endale <onyxbngame@gmail.com>
2025-03-21 13:46:49 +0300

    New dix v6

--- 5th project/task.js
+++ 5th project/task.js
@@ -2,6 +2,35 @@ const addBtn = document.getElementById("addBtn");
 let textarea = document.getElementById("txtArea");
 
 addBtn.addEventListener("click", function () {
-  const x = textarea.value;
-  console.log(x);
+  const container = document.querySelector(".added_tasks");
+  let taskContainer = document.createElement("div");
+  taskContainer.classList.add("taskContainer");
+  let li = document.createElement("li");
+  li.innerText = `${textarea.value}`;
+  li.classList.add("taskList");
+
+  taskContainer.appendChild(li);
+
+  let CDBtn = document.createElement("div");
+  CDBtn.classList.add("taskButtons");
+
+  const checkBtn = document.createElement("button");
+  checkBtn.classList.add("checkBtn");
+  checkBtn.innerHTML = '<i class="fa-solid fa-check"></i>';
+
+  CDBtn.appendChild(checkBtn);
+
+  const deleteBtn = document.createElement("button");
+  deleteBtn.innerHTML = '<i class="fa-solid fa-trash-can"></i>';
+  deleteBtn.classList.add("deleteBtn");
+  CDBtn.appendChild(deleteBtn);
+
+  taskContainer.appendChild(CDBtn);
+
+  if (textarea.value == "") {
+    window.alert("You didnt put any task");
+  } else {
+    container.appendChild(taskContainer);
+  }
+  textarea.value = null;
 });
